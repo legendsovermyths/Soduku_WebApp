691164a1e7a69becb921151eab86e8f51d8dff0e
Mask Wolf <maskwolf2000@gmail.com>
2020-12-27 04:31:37 +0530

    sudoku grid added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "./Navbar";
 import "./App.css";
+import SudokuGrid from "./SudokuGrid";
 
 function App() {
   return (
     <div className='App'>
       <Navbar></Navbar>
+      <SudokuGrid></SudokuGrid>
     </div>
   );
 }
