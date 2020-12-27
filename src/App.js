26acd23632b6fb1f736182f9ef003b804655a7e8
Mask Wolf <maskwolf2000@gmail.com>
2020-12-27 22:40:07 +0530

    grid is now done

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import React from "react";
 import Navbar from "./Navbar";
 import "./App.css";
 import SudokuGrid from "./SudokuGrid";
+import Button from "@material-ui/core/Button";
 
 function App() {
   return (
     <div className='App'>
       <Navbar></Navbar>
-      <SudokuGrid></SudokuGrid>
+      <div className='sudokubar'>
+        <div className='sudoku'>
+          <SudokuGrid></SudokuGrid>
+        </div>
+        <div className='timer'>
+          <Button variant='contained' color='primary' disableElevation>
+            Solve
+          </Button>
+        </div>
+      </div>
     </div>
   );
 }
--- src/SudokuGrid.js
+++ src/SudokuGrid.js
@@ -1,272 +1,42 @@
 // style={"border-collapse: collapse;"}
 import React from "react";
 import "./SudokuGrid.css";
+var arr = [1, 10, 19, 28, 37, 46, 55, 64, 73];
 function SudokuGrid() {
   return (
     <div class='SudokuGrid'>
       <table id='grid' border='1'>
-        <tr class='row'>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-        </tr>
-        <tr class='row'>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-        </tr>
-        <tr class='row'>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-        </tr>
-
-        <tr class='row'>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-        </tr>
-        <tr class='row'>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-        </tr>
-        <tr class='row'>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-        </tr>
-        <tr class='row'>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-        </tr>
-        <tr class='row'>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-        </tr>
-        <tr class='row'>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-          <td class='cell'>
-            <input type='text' maxlength='1' />
-          </td>
-        </tr>
+        {arr.map((item, index) => (
+          <tr class='row' id={item}>
+            <td class='cell'>
+              <input type='text' maxlength='1' id={item} />
+            </td>
+            <td class='cell'>
+              <input type='text' maxlength='1' id={item + 1} />
+            </td>
+            <td class='cell'>
+              <input type='text' maxlength='1' id={item + 2} />
+            </td>
+            <td class='cell'>
+              <input type='text' maxlength='1' id={item + 3} />
+            </td>
+            <td class='cell'>
+              <input type='text' maxlength='1' id={item + 4} />
+            </td>
+            <td class='cell'>
+              <input type='text' maxlength='1' id={item + 5} />
+            </td>
+            <td class='cell'>
+              <input type='text' maxlength='1' id={item + 6} />
+            </td>
+            <td class='cell'>
+              <input type='text' maxlength='1' id={item + 7} />
+            </td>
+            <td class='cell'>
+              <input type='text' maxlength='1' id={item + 8} />
+            </td>
+          </tr>
+        ))}
       </table>
     </div>
   );
