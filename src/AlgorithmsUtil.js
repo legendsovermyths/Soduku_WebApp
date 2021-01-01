086f7053b9953d68c794478030ae5bea1e142108
Mask Wolf <maskwolf2000@gmail.com>
2021-01-01 23:17:49 +0530

    algoritms added

---
+++ src/AlgorithmsUtil.js
@@ -0,0 +1,85 @@
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const shuffle = function shuffle(array) {
+  var m = array.length,
+    t,
+    i;
+  while (m) {
+    i = Math.floor(Math.random() * m--);
+    t = array[m];
+    array[m] = array[i];
+    array[i] = t;
+  }
+
+  return array;
+};
+const fillDiagonal = function fillDiagonal(SudokuGrid) {
+  const array1 = shuffle(numbers);
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      SudokuGrid[i][j] = array1[3 * i + j];
+    }
+  }
+  const array2 = shuffle(numbers);
+  for (let i = 3; i < 6; i++) {
+    for (let j = 3; j < 6; j++) {
+      SudokuGrid[i][j] = array2[3 * (i - 3) + (j - 3)];
+    }
+  }
+  const array3 = shuffle(numbers);
+  for (let i = 6; i < 9; i++) {
+    for (let j = 6; j < 9; j++) {
+      SudokuGrid[i][j] = array3[3 * (i - 6) + (j - 6)];
+    }
+  }
+  return SudokuGrid;
+};
+function isValid(board, row, col, k) {
+  for (let i = 0; i < 9; i++) {
+    const m = 3 * Math.floor(row / 3) + Math.floor(i / 3);
+    const n = 3 * Math.floor(col / 3) + (i % 3);
+    if (board[row][i] === k || board[i][col] === k || board[m][n] === k) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const sodokoSolver = function sodokoSolver(SudokuGrid) {
+  for (let i = 0; i < 9; i++) {
+    for (let j = 0; j < 9; j++) {
+      if (SudokuGrid[i][j] === 0) {
+        for (let k = 1; k <= 9; k++) {
+          if (isValid(SudokuGrid, i, j, k)) {
+            SudokuGrid[i][j] = k;
+            if (sodokoSolver(SudokuGrid)) {
+              return true;
+            } else {
+              SudokuGrid[i][j] = 0;
+            }
+          }
+        }
+        return false;
+      }
+    }
+  }
+  return true;
+};
+const removeKdigits = function removeKDigits(k, sudokuGrid) {
+  var count = k;
+  while (count !== 0) {
+    var cellId = Math.floor(Math.random() * 81);
+    var row = Math.floor(cellId / 9);
+    var col = Math.floor(cellId % 9);
+    if (col !== 0) {
+      col = col - 1;
+    }
+    if (sudokuGrid[row][col] !== 0) {
+      count--;
+      sudokuGrid[row][col] = 0;
+    }
+  }
+};
+// let array = Array(9)
+//   .fill()
+//   .map(() => Array(9).fill(0));
+export { fillDiagonal, sodokoSolver, removeKdigits };
--- src/SudokuGrid.js
+++ src/SudokuGrid.js
@@ -1,39 +1,54 @@
 // style={"border-collapse: collapse;"}
-import React from "react";
+import React, { useState } from "react";
+import { fillDiagonal, sodokoSolver, removeKdigits } from "./AlgorithmsUtil";
 import "./SudokuGrid.css";
-var arr = [1, 10, 19, 28, 37, 46, 55, 64, 73];
+
 function SudokuGrid() {
+  var arr = [0, 9, 18, 27, 36, 45, 54, 63, 72];
+  var grid2 = Array(9)
+    .fill()
+    .map(() => Array(9).fill(0));
+  grid2 = fillDiagonal(grid2);
+  sodokoSolver(grid2);
+  removeKdigits(9, grid2);
+  const [grid, setGrid] = useState(grid2);
   return (
     <div class='SudokuGrid'>
       <table id='grid' border='1'>
         {arr.map((item, index) => (
           <tr class='row' id={item}>
             <td class='cell'>
-              <input type='text' maxlength='1' id={item} />
+              <input
+                type='text'
+                maxlength='1'
+                value={grid[0][0]}
+                id={index}
+                key={item}
+              />
             </td>
             <td class='cell'>
-              <input type='text' maxlength='1' id={item + 1} />
+              <input type='text' maxlength='1' id={item + 1} key={item + 1} />
             </td>
             <td class='cell'>
-              <input type='text' maxlength='1' id={item + 2} />
+              <input type='text' maxlength='1' id={item + 2} key={item + 2} />
             </td>
             <td class='cell'>
-              <input type='text' maxlength='1' id={item + 3} />
+              <input type='text' maxlength='1' id={item + 3} key={item + 3} />
             </td>
             <td class='cell'>
-              <input type='text' maxlength='1' id={item + 4} />
+              <input type='text' maxlength='1' id={item + 4} key={item + 4} />
             </td>
             <td class='cell'>
-              <input type='text' maxlength='1' id={item + 5} />
+              <input type='text' maxlength='1' id={item + 5} key={item + 5} />
             </td>
             <td class='cell'>
-              <input type='text' maxlength='1' id={item + 6} />
+              <input type='text' maxlength='1' id={item + 6} key={item + 6} />
             </td>
             <td class='cell'>
-              <input type='text' maxlength='1' id={item + 7} />
+              <input type='text' maxlength='1' id={item + 7} key={item + 7} />
             </td>
             <td class='cell'>
-              <input type='text' maxlength='1' id={item + 8} />
+              <input type='text' maxlength='1' id={item + 8} key={item + 8} />
             </td>
           </tr>
         ))}
